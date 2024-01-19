5072ef5670211a0335880c8c7c8db77766adcb2b
Lucyna Śmieszek <smieszek.lucyna@gmail.com>
2024-01-19 20:45:43 +0100

    Listy , tablice , for, foreach

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] grades = new int[365];
+List<string> daysOfWeek = new List<string>();
+daysOfWeek.Add("poniedziałek");
+daysOfWeek.Add("wtorek");
+daysOfWeek.Add("środa");
+daysOfWeek.Add("czwartek");
+daysOfWeek.Add("piątek");
+daysOfWeek.Add("sobota");
+daysOfWeek.Add("niedziela");
+
+
+for (var i=0; i<daysOfWeek.Count; i++)
+{
+    Console.WriteLine(daysOfWeek[i]);
+}
+
+foreach( var day in daysOfWeek)
+{
+    Console.WriteLine(day);
+}
+
+
